321422992f6ddfa8a3c4323d22b8ff5c662b7ecf
Priyanshi Nigam <priyaaanigam.pn@gmail.com>
2020-09-17 03:32:15 -0700

    Added ChatBox

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,7 +2,33 @@ import React, { Component } from "react";
 
 class MainContainer extends Component {
   render() {
-    return <div className="MainCont">gell</div>;
+    return (
+      <div id="Main">
+        <div className="about-channel">
+          <div className="name-channel"># Announcment</div>
+          <div className="desc-channel">Something</div>
+        </div>
+        <div className="messages-list">
+          <div className="message">
+            <div className="left-block">
+              <img
+                src="https://ca.slack-edge.com/T0188513NTW-U01867WD8GK-ga631e27835b-72"
+                alt="pic"
+              />
+            </div>
+            <div className="right-block">
+              <div className="user">
+                Priyanshi <span>1:21 PM</span>
+                <div>Hey</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="input">
+          <textarea placeholder="Type Something and press enter...."></textarea>
+        </div>
+      </div>
+    );
   }
 }
 
