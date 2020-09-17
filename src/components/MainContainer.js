7d9340f48c793f1d7c27a33f43148aa4a46dc2fa
Priyanshi Nigam <priyaaanigam.pn@gmail.com>
2020-09-17 01:46:58 -0700

    Styled Sidebar, SignIn Page"

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class MainContainer extends Component {
   render() {
-    return <div>MainContainer</div>;
+    return <div className="MainCont">gell</div>;
   }
 }
 
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -5,7 +5,17 @@ class SignIn extends Component {
     return (
       <div className="signin-form">
         <h1>Sign In/Sign Up</h1>
-        <button>Sign In/Sign Up with Goggle</button>
+        <button className="btn btn-basic">
+          <img src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Google-256.png" />
+          Sign In with Goggle
+        </button>
+
+        <div style={{ textAlign: "center", fontSize: 13 }}>OR</div>
+
+        <button className="btn btn-basic">
+          <img src="https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Google-256.png" />
+          Sign Up with Google
+        </button>
       </div>
     );
   }
